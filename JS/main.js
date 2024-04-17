395a9bc243703055b962fdd89bf5d5ed1a64e878
Gabriel Bento <165726325+Bento777777@users.noreply.github.com>
2024-04-17 20:55:41 -0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,61 +1,61 @@
-/*================ NAVBAR============= */
+/* ====================== NAVBAR ===================*/
 
 let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar')
+let navbar = document.querySelector('.navbar');
 
-menuIcon.onclik = () => {
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active')
 }
 
-/*================ ACTIVE LINK============= */
+/* ====================== ACTIVE LINK ===================*/
 
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a')
+let navLinks = document.querySelectorAll('head nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
-        let height =sec.offsetHeight;
+        let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height){
             navLinks.forEach.apply(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').
-                classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
 
-    /*================ STICKY NAVBAR============= */
-    let header = document.querySelectorAll('header');
-    header.classList.toggle('sticky' ,window.scrollY > 100);
+    /* ====================== STICKY NAVBAR ===================*/
 
-    /*================ REMOVE NAVBAR============= */
-    menuIcon.classList.remove('fa-xmark');
-    navbar.classList.remove('active');
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+      /* ====================== REMOVE NAVBAR ===================*/
+
+menuIcon.classList.remove('fa-xmark');
+navbar.classList.remove('active');
 };
 
-/*================ SCROLL REVEAL ============= */
+  /* ====================== SCROLL REVEAL ===================*/
 
-scrollReveal({
+  ScrollReveal({
     distance: '80px',
     duration: 2000,
     delay: 200,
-});
+  });
 
-scrollReveal().reveal('.home-content, heading', {origin:'top'});
-scrollReveal().reveal('.home-img, .services-container, .portifolio-box, . contact form', {origin:'bottom'});
-scrollReveal().reveal('.home-contact h1, .about-img', {origin:'left'});
-scrollReveal().reveal(',home-contact p, .about-content', {origin:'righ'});
+  ScrollReveal().reveal('.home-content, heading', {origin:'top'});
+  ScrollReveal().reveal('.home-img, .services-container, .portifolio-box, . contact form', {origin:'bottom'});
+  ScrollReveal().reveal('.home-contact h1, .about-img', {origin:'left'});
+  ScrollReveal().reveal('.home-contact p, .about-content', {origin:'right'});
 
-    const typed = new Typed('.multiple-text', {
-        strings:['desenvolvedor web', 'web designer','Youtuber'],
-        typeSpeed: 70,
-        backSpeed: 70,
-        backDelat: 1000,
-        loop: true,
-
-    })
+  const typed = new Typed('.multiple-text', {
+      strings:['Desenvolvedor Web', 'Técnico em Informática', 'Gamer'],
+      typeSpeed: 70,
+      backSpeed: 70,
+      backDelat: 1000,
+      loop: true,
+  })
